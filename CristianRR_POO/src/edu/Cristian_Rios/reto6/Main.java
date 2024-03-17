187aff60d3f58554a94c536ee27db13a182e798e
NOTCRISTIAN123 <cristianignaciorios@gmail.com>
2024-03-16 22:53:32 -0600

    Reto 6:
    - Tested-by: Cristian Rios (Creator)
    - Reviewed-by: Cristian Rios (Creator)
    - Estatus: Inclomplete (60% left)
    - Function: This is a calculator that has the next functions: addition, subtraction, multiplication, division, module, logarithm, power, square root.
    - Other: This is a school proyect.

---
+++ CristianRR_POO/src/edu/Cristian_Rios/reto6/Main.java
@@ -0,0 +1,10 @@
+// Archivo Main.java
+package edu.Cristian_Rios.reto6;
+
+import edu.Cristian_Rios.reto6.ui.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        CLI.mostrarMenu(); // Iniciar el menú CLI
+    }
+}
---
+++ CristianRR_POO/src/edu/Cristian_Rios/reto6/process/calculos.java
@@ -0,0 +1,9 @@
+// Archivo calculos.java
+package edu.Cristian_Rios.reto6.process;
+
+public class calculos {
+    public static void iniciarCalculadora(int calculomodo) {
+        System.out.println("La calculadora se está iniciando en modo: " + calculomodo);
+        // Aquí puedes implementar la lógica para cada modo de cálculo
+    }
+}
