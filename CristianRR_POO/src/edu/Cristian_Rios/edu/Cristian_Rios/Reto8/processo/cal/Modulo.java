2dbe0e3cfb7285aea6273fbcf3fe048884f0ed98
NOTCRISTIAN123 <cristianignaciorios@gmail.com>
2024-03-23 20:49:17 -0600

    kjhkj

---
+++ CristianRR_POO/src/edu/Cristian_Rios/edu/Cristian_Rios/Reto8/processo/cal/Modulo.java
@@ -0,0 +1,15 @@
+package edu.Cristian_Rios.Reto7.process.cal;
+
+import edu.Cristian_Rios.Reto7.process.cal.Resta;
+
+public class Modulo extends Resta {
+    public Modulo(float x, float y) {
+        super(x, y);
+    }
+
+    @Override
+    public void calcular() {
+        resultado = x % y;
+        System.out.println("El resultado del módulo es: " + resultado);
+    }
+}
